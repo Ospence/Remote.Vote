2cf6a3c46237dc3284c5ae9544aba617a5d58342
Juli Orms <juli15dawn@hotmail.com>
2015-11-09 11:35:34 -0600

    wrote angular app user add update

---
+++ VotingApp/VotingApp/Scripts/ngApp/RegisterController.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    angular.module('VotingApp')
+    .controller('RegisterController', function (RegisterService, $location) {
+        var vm = this;
+
+        vm.applicationusers = RegisterService.list();
+
+        vm.add = function () {
+            RegisterService.add(vm.newApplicationUser, function (result) {
+                vm.e.push(result);
+                $('#addApplicationUser')[0].reset();
+                //$location.path('/register');
+            }); 
+
+        };
+
+        vm.update = function (applicationUser) {
+            RegisterService.update(applicationUser);
+        };
+
+       // vm.delete = function (applicationUser) {
+            //RegisterService.delete(applicaitonUser, function () {
+                //vm.applicationUsers = vm.appllicationUsers.filter(function (e) {
+                    //return e.id != applicationUser.id;
+              //  });
+            });
+        
+
+})();
