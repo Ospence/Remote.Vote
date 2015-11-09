8d004fe98f39a1a6583c6c9adbcf4d48b93cbb99
Joseph Cheung <jcheung375@gmail.com>
2015-11-09 11:41:38 -0600

    registerService

---
+++ VotingApp/VotingApp/Scripts/ngApp/registerService.js
@@ -0,0 +1,27 @@
+﻿(function () {
+
+    angular.module('VotingApp')
+        .service('registerService', ['$resource', function ($resource) {
+            var self = this;
+
+            var ApplicationUser = $resource('/api/applicationuser/:id');
+
+            self.list = function () {
+                return ApplicationUser.query();
+            };
+
+            self.add = function (applicationUser, callback) {
+                var newApplicationUser = new ApplicationUser({
+                    firstName: applicationUser.firstName,
+                    lastName: applicationUser.lastName,
+                    email: applicationUser.email,
+                    phoneNumber: applicationUser.phoneNumber
+                });
+                newApplicationUser.$save(callback);
+            };
+
+            self.update = function (applicationUser) {
+                applicationUser.$save();
+            };
+        }]);
+})();
